40869a1fe1dc49d4a26f23bf36b27e9adcee2011
Phuc Tran <glup3.tran@gmail.com>
2024-07-05 10:43:57 +0200

    use snapshots in test

--- internal/repository/repo_repository_test.go
+++ internal/repository/repo_repository_test.go
@@ -4,23 +4,64 @@ import (
 	"context"
 	"testing"
 
+	database "github.com/glup3/TrendyGitHub/internal/db"
 	"github.com/glup3/TrendyGitHub/internal/repository"
 	"github.com/glup3/TrendyGitHub/internal/testutil"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func TestRepoRepository_Test1(t *testing.T) {
-	ctx := context.Background()
-
-	db, cleanup, err := testutil.SetupPostgresContainer(ctx)
+func TestRepoRepository(t *testing.T) {
+	connString, cleanup, restore, err := testutil.SetupPostgresContainer()
 	if err != nil {
 		t.Fatalf("failed to set up test container: %v", err)
 	}
 	defer cleanup()
 
-	r := repository.NewRepoRepository(ctx, db)
+	t.Run("Test finding next missing repo asc", func(t *testing.T) {
+		t.Cleanup(func() {
+			restore()
+		})
 
-	_, err = r.FindNextMissing(100, repository.OrderAsc)
-	if err == nil {
-		t.Fatal("expected error")
-	}
+		ctx := context.Background()
+		pool, err := pgxpool.New(ctx, connString)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer pool.Close()
+
+		r := repository.NewRepoRepository(ctx, &database.Database{Pool: pool})
+
+		repo, err := r.FindNextMissing(1_000_000, repository.OrderAsc)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if repo.Id != 1 {
+			t.Fatalf("Expected %d to equal 1", repo.Id)
+		}
+	})
+
+	t.Run("Test finding next missing desc", func(t *testing.T) {
+		t.Cleanup(func() {
+			restore()
+		})
+
+		ctx := context.Background()
+		pool, err := pgxpool.New(ctx, connString)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer pool.Close()
+
+		r := repository.NewRepoRepository(ctx, &database.Database{Pool: pool})
+
+		repo, err := r.FindNextMissing(1_000_000, repository.OrderDesc)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if repo.Id != 6 {
+			t.Fatalf("Expected %d to equal 6", repo.Id)
+		}
+	})
 }
--- internal/testutil/postgres_testutil.go
+++ internal/testutil/postgres_testutil.go
@@ -8,17 +8,19 @@ import (
 	"path/filepath"
 	"time"
 
-	"github.com/glup3/TrendyGitHub/internal/db"
+	sq "github.com/Masterminds/squirrel"
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
 	"github.com/testcontainers/testcontainers-go/wait"
 )
 
-func SetupPostgresContainer(ctx context.Context) (*db.Database, func(), error) {
-	postgresContainer, err := postgres.RunContainer(ctx,
+func SetupPostgresContainer() (string, func(), func(), error) {
+	ctx := context.Background()
+	container, err := postgres.RunContainer(ctx,
 		postgres.WithDatabase("tgh"),
 		postgres.WithUsername("ditto"),
 		postgres.WithPassword("blubblub"),
@@ -28,18 +30,12 @@ func SetupPostgresContainer(ctx context.Context) (*db.Database, func(), error) {
 				WithStartupTimeout(10*time.Second)),
 	)
 	if err != nil {
-		return nil, nil, fmt.Errorf("failed to start container: %w", err)
+		return "", nil, nil, fmt.Errorf("failed to start container: %w", err)
 	}
 
-	connStr, err := postgresContainer.ConnectionString(ctx, "sslmode=disable")
+	connString, err := container.ConnectionString(ctx, "sslmode=disable")
 	if err != nil {
-		return nil, nil, fmt.Errorf("failed to get connection string: %w", err)
-	}
-
-	database, err := db.NewDatabase(ctx, connStr)
-	if err != nil {
-		postgresContainer.Terminate(ctx)
-		return nil, nil, fmt.Errorf("failed to create database pool: %w", err)
+		return "", nil, nil, fmt.Errorf("failed to get connection string: %w", err)
 	}
 
 	projectRoot, err := findProjectRoot()
@@ -48,25 +44,84 @@ func SetupPostgresContainer(ctx context.Context) (*db.Database, func(), error) {
 	}
 
 	migrationsPath := filepath.Join(projectRoot, "db", "migrations")
-	m, err := migrate.New("file://"+migrationsPath, connStr)
+	m, err := migrate.New("file://"+migrationsPath, connString)
 	if err != nil {
-		postgresContainer.Terminate(ctx)
-		return nil, nil, fmt.Errorf("failed to create migrate instance: %w", err)
+		container.Terminate(ctx)
+		return "", nil, nil, fmt.Errorf("failed to create migrate instance: %w", err)
 	}
 
 	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-		postgresContainer.Terminate(ctx)
-		return nil, nil, fmt.Errorf("failed to apply migrations: %w", err)
+		container.Terminate(ctx)
+		return "", nil, nil, fmt.Errorf("failed to apply migrations: %w", err)
+	}
+
+	err = seedDatabase(ctx, connString)
+	if err != nil {
+		return "", nil, nil, fmt.Errorf("failed to seed database: %w", err)
+	}
+
+	srcErr, dbErr := m.Close()
+	if srcErr != nil {
+		return "", nil, nil, srcErr
+	}
+	if dbErr != nil {
+		return "", nil, nil, dbErr
+	}
+
+	err = container.Snapshot(ctx, postgres.WithSnapshotName("test-snapshot"))
+	if err != nil {
+		return "", nil, nil, fmt.Errorf("failed to take snapshot: %w", err)
+	}
+
+	connString, err = container.ConnectionString(ctx, "sslmode=disable")
+	if err != nil {
+		return "", nil, nil, fmt.Errorf("failed to get connection string: %w", err)
+	}
+
+	restore := func() {
+		err := container.Restore(ctx)
+		if err != nil {
+			log.Fatalf("failed to restore container: %s", err)
+		}
 	}
 
 	cleanup := func() {
-		database.Close()
-		if err := postgresContainer.Terminate(ctx); err != nil {
+		if err := container.Terminate(ctx); err != nil {
 			log.Fatalf("failed to terminate container: %s", err)
 		}
 	}
 
-	return database, cleanup, nil
+	return connString, cleanup, restore, nil
+}
+
+func seedDatabase(ctx context.Context, connString string) error {
+	pool, err := pgxpool.New(ctx, connString)
+	if err != nil {
+		return err
+	}
+	defer pool.Close()
+
+	sql, args, err := sq.
+		Insert("repositories").
+		Columns("id", "github_id", "name", "name_with_owner", "star_count", "fork_count", "primary_language", "history_missing", "languages").
+		Values(0001, "R_kg0001", "glup3", "glup3/repo0001", 200, 0, "Go", true, []string{}).
+		Values(0002, "R_kg0002", "glup3", "glup3/repo0002", 400, 0, "Go", true, []string{}).
+		Values(0003, "R_kg0003", "glup3", "glup3/repo0003", 400_000, 0, "Go", false, []string{}).
+		Values(0004, "R_kg0004", "glup3", "glup3/repo0004", 30_000, 0, "Go", true, []string{}).
+		Values(0005, "R_kg0005", "glup3", "glup3/repo0005", 1000, 0, "Go", true, []string{}).
+		Values(0006, "R_kg0006", "glup3", "glup3/repo0006", 84_000, 0, "Go", true, []string{}).
+		PlaceholderFormat(sq.Dollar).
+		ToSql()
+	if err != nil {
+		return err
+	}
+
+	_, err = pool.Exec(ctx, sql, args...)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func findProjectRoot() (string, error) {
